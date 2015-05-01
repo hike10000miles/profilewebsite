6b4410e936ee979ea48520bc3e531719d54efad0
hike10000miles <chenshen4009@gmail.com>
2015-05-01 18:19:31 -0400

    div history need more work.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,6 @@
     $('.item2').hover(function() {
       $(this).toggleClass('hightlight2');
     });
-    //$('#toggleButton').on('click', function() {
-    	//$('.toggleMenu').toggleClass('is-active');
-    //});
     $(window).scroll(function() {
     	if ($(this).scrollTop() > 643){
     		$('.nav').addClass('is-fixed');
