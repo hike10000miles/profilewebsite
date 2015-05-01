3591963edf90a808dcad90bdfe8e0c9a91d4f8d4
hike10000miles <chenshen4009@gmail.com>
2015-04-30 22:29:43 -0400

    worked out most of the structure, started working on responsive design.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,24 @@
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
-
+    $('.item').hover(function() {
+      $(this).toggleClass('hightlight');
+      });
+    $('.item2').hover(function() {
+      $(this).toggleClass('hightlight2');
+    });
+    //$('#toggleButton').on('click', function() {
+    	//$('.toggleMenu').toggleClass('is-active');
+    //});
+    $(window).scroll(function() {
+    	if ($(this).scrollTop() > 643){
+    		$('.nav').addClass('is-fixed');
+    	} else {
+    		$('.is-fixed').removeClass('is-fixed');
+    	}
+    });
+    $('#toggleButton').on('click', function() {
+    	$('.nav').find('.toggleMenu').fadeToggle(400);
+    });
   });
 })(jQuery);
