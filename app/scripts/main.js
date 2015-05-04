5daecce5c41b9c9a7ac7e915a9d3df0a6de532e3
hike10000miles <chenshen4009@gmail.com>
2015-05-04 14:58:18 -0400

    fixed link.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,5 +17,17 @@
     $('#toggleButton').on('click', function() {
     	$('.nav').find('.toggleMenu').fadeToggle(300);
     });
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 })(jQuery);
