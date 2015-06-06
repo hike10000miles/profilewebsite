f144d1c349fca08be8611507790ee92150565ec8
hike10000miles <chenshen4009@gmail.com>
2015-06-06 14:14:46 -0400

    added in Waypoint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,31 +3,28 @@
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
-    var fullHeight = window.screen.height;
+    //var fullHeight = window.screen.height;
     $('.item').hover(function() {
       $(this).toggleClass('hightlight');
       });
-    $(window).scroll(function() {
-    	if ($(this).scrollTop() > fullHeight){
-    		$('.nav').addClass('is-fixed');
-    	} else {
-    		$('.is-fixed').removeClass('is-fixed');
-    	}
-    });
     $('#toggleButton').on('click', function() {
     	$('.nav').find('.toggleMenu').fadeToggle(300);
     });
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
+    //$('a[href*=#]:not([href=#])').click(function() {
+      //if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        //var target = $(this.hash);
+        //target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        //if (target.length) {
+          //$('html,body').animate({
+            //scrollTop: target.offset().top
+          //}, 1000);
+          //return false;
+        //}
+      //}
+    //});
+    var sticky = new Waypoint.Sticky({
+      element: $('.nav')[0]
     });
+    sticky();
   });
 })(jQuery);
